45fcba92554a67e7556eeb0e72130b10b29c9750
yraghuwanshi124-droid <yraghuwanshi124@gmail.com>
2026-02-13 15:14:35 +0530

    first game

---
+++ game.c
@@ -0,0 +1,117 @@
+// 1.                                                    SNAKE WATER AND GUN
+
+ 
+
+
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <time.h>
+
+// int main()
+// {
+
+//     srand(time(0));
+//     int player,computer = (rand() % 3);
+
+//     printf("choose 0 for snake,1 for water and 2 for gun\n");
+//     scanf("%d",&player);
+//     printf("%d\n",computer);
+
+
+    
+// /*
+//     0 -- snake 
+//     1 -- water
+//     2 -- gun
+
+// */
+
+//    if (player == 0 && computer ==0){
+//     printf("it's a draw please try again\n");
+//    }
+//    else if (player == 0 && computer ==1){
+//     printf("you win\n");
+//    }
+//    else if (player == 0 && computer ==2){
+//     printf("you loose!\n");
+//    }
+//    else if (player == 1 && computer ==0){
+//     printf("you loose!");
+//    }
+//    else if (player == 1 && computer ==1){
+//     printf("it's a draw please try again\n");
+//    }
+//    else if (player == 1 && computer ==2){
+//     printf("you win\n");
+//    }
+//    else if (player == 2 && computer ==0){
+//     printf("you win\n");
+//    }
+//    else if (player == 2 && computer ==1){
+//     printf("you loose!\n");
+//    }
+//    else if (player == 2 && computer ==2){
+//     printf("it's a draw please try again\n");
+//    }
+
+// }
+
+
+
+// 2.                                                 #  STONE PAPPER AND SCISSORS # 
+
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <time.h>
+
+// int main()
+// {
+
+//     srand(time(0));
+//     int player,computer = (rand() % 3);
+
+//     printf("choose 0 for stone,1 for paper and 2 for sesor\n");
+//     scanf("%d",&player);
+//     int game = player;
+//     printf("you choose:%d\n",game);
+
+//     printf("computer choose %d\n",computer);
+
+
+    
+// /*
+//     0 -- stone
+//     1 -- paper
+//     2 -- sesors
+
+// */
+
+//    if (game == 0 && computer ==0){
+//     printf("it's a draw please try again\n");
+//    }
+//    else if (game == 0 && computer ==1){
+//     printf("you loose!\n");
+//    }
+//    else if (game == 0 && computer ==2){
+//     printf("you win\n");
+//    }
+//    else if (game == 1 && computer ==0){
+//     printf("you win");
+//    }
+//    else if (game == 1 && computer ==1){
+//     printf("it's a draw please try again\n");
+//    }
+//    else if (game == 1 && computer ==2){
+//     printf("you loose!\n");
+//    }
+//    else if (game == 2 && computer ==0){
+//     printf("you loose!\n");
+//    }
+//    else if (game == 2 && computer ==1){
+//     printf("you win\n");
+//    }
+//    else if (game == 2 && computer ==2){
+//     printf("it's a draw please try again\n");
+//    }
+
+// }
